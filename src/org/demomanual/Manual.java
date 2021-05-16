cb4d3ad4bb0d5632329b8c0d18d0bd6a51901f34
hemagayathri97 <hemagaythri97@gmail.com>
2021-05-16 18:52:11 +0530

    added to a branch

--- src/org/demomanual/Manual.java
+++ src/org/demomanual/Manual.java
@@ -3,6 +3,7 @@ package org.demomanual;
 public class Manual {
 	private void softwareTesting() {
 System.out.println("Software Testing ");
+System.out.println("testing");
 	}
 	private void hardwareTesting() {
 System.out.println("Hardware Testing ");
